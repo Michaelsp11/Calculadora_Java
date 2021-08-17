8358726bf2838a938e974a730241fb40982ce6df
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-08-17 14:13:18 +0200

    Métodos de los cálculos de la calculadora más modularizado + testeo de las operaciones de la calculadora.

--- src/practica1/Practica1.java
+++ src/practica1/Practica1.java
@@ -13,69 +13,85 @@ import Util.Util;
  */
 public class Practica1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Bienvenido!");
-        int option = 0;
-        do {
-            option = seleccionaOpcion();
-            if (option < 5) {
-                double result = realizarOperacion(option);
-                pintarResultado(result);
-            }
-        } while (option < 5);
-        System.out.println("Hasta pronto!");
-    }
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Bienvenido!");
+		int opcion = 0;
+		do {
+			opcion = seleccionaOpcion();
+			if (opcion < 5) {
+				double result = realizarOperacion(opcion);
+				pintarResultado(result);
+			}
+		} while (opcion < 5);
+		System.out.println("Hasta pronto!");
+	}
 
-    public static int seleccionaOpcion() {
-        int option = 0;
-        do {
-            System.out.println("Que operaci贸n desea realizar?");
-            System.out.println("1. Sumar");
-            System.out.println("2. Restar");
-            System.out.println("3. Multiplicar");
-            System.out.println("4. Dividir");
-            System.out.println("5. Salir");
-            option = Util.pedirInt("Escoge una opci贸n: ");
-            if (option < 1 || option > 5) {
-                System.out.println("Opci贸n erronia!!");
-            }
-        } while (option < 1 || option > 5);
-        return option;
-    }
+	public static int seleccionaOpcion() {
+		int opcion = 0;
+		do {
+			System.out.println("Que operaci髇 desea realizar?");
+			System.out.println("1. Sumar");
+			System.out.println("2. Restar");
+			System.out.println("3. Multiplicar");
+			System.out.println("4. Dividir");
+			System.out.println("5. Salir");
+			opcion = Util.pedirInt("Escoge una opci髇: ");
+			if (opcion < 1 || opcion > 5) {
+				System.out.println("Opci髇 erronia!!");
+			}
+		} while (opcion < 1 || opcion > 5);
+		return opcion;
+	}
 
-    public static double realizarOperacion(int operacion) {
-        double result = 0;
-        double primerNumero = Util.pedirDouble("Primer numero de la operaci贸n:");
-        double segundoNumero = Util.pedirDouble("Segundo numero de la operaci贸n:");
-        switch (operacion) {
-            case 1:
-                result = primerNumero + segundoNumero;
-                break;
-            case 2:
-                result = segundoNumero < 0? primerNumero + segundoNumero : primerNumero - segundoNumero;
-                break;
-            case 3:
-                result = segundoNumero < 0? primerNumero + segundoNumero : primerNumero * segundoNumero;
-                break;
-            case 4:
-                if (segundoNumero == 0) {
-                    System.out.println("El divissor tiene que ser diferente de 0!");
-                    realizarOperacion(operacion);
-                } else {
-                    result = segundoNumero < 0 ? primerNumero + segundoNumero : primerNumero / segundoNumero; 
-                }
-                break;
-            default:
-                break;
-        }
+	public static double realizarOperacion(int operacion) {
+		double result = 0;
+		double primerNumero = Util.pedirDouble("Primer numero de la operaci髇:");
+		double segundoNumero = Util.pedirDouble("Segundo numero de la operaci髇:");
+		switch (operacion) {
+		case 1:
+			result = suma(primerNumero, segundoNumero);
+			break;
+		case 2:
+			result = resta(primerNumero, segundoNumero);
+			break;
+		case 3:
+			result = multiplicacion(primerNumero, segundoNumero);
+			break;
+		case 4:
+			if (segundoNumero == 0) {
+				System.out.println("El divissor tiene que ser diferente de 0!");
+				realizarOperacion(operacion);
+			} else {
+				result = division(primerNumero, segundoNumero);
+			}
+			break;
+		default:
+			break;
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    public static void pintarResultado(double resultado) {
-        System.out.printf("El resultado final de la operaci贸n es %.2f\n", resultado);
-    }
+	public static double suma(double primerNumero, double segundoNumero) {
+		return primerNumero + segundoNumero;
+	}
+
+	public static double resta(double primerNumero, double segundoNumero) {
+		return segundoNumero < 0 ? suma(primerNumero, segundoNumero) : primerNumero - segundoNumero;
+	}
+
+	public static double multiplicacion(double primerNumero, double segundoNumero) {
+		return segundoNumero < 0 ? suma(primerNumero, segundoNumero) : primerNumero * segundoNumero;
+	}
+
+	public static double division(double primerNumero, double segundoNumero) {
+		return segundoNumero < 0 ? suma(primerNumero, segundoNumero) : primerNumero / segundoNumero;
+	}
+
+	public static void pintarResultado(double resultado) {
+		System.out.printf("El resultado final de la operaci髇 es %.2f\n", resultado);
+	}
 }
